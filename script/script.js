172e7c38ecd26400f3438b4b534be4297ca768b2
gianella <g.cevallos@gianellin.com>
2023-02-19 00:13:53 -0500

    update nav

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
 // add class navbarLight on navbar scroll
 const header = document.querySelector('.navbar');
+console.log(header)
 
 window.onscroll = function() {
-    var top = window.scrollY;
+    const top = window.scrollY;
     if(top >=100) {
-        header.classList.add('navbarLight');
+        header.classList.add('navbarDark');
     }
     else {
-        header.classList.remove('navbarLight');
+        header.classList.remove('navbarDark');
     }
 }
 
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
 
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
